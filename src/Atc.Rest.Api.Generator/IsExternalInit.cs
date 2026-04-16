46dbf585c4fcaea87d9579e3e85c7ae690b2d695
davidkallesen <david@lmdk.dk>
2026-04-16 09:52:33 +0200

    style: apply linter auto-fixes (SuppressMessage imports)

--- src/Atc.Rest.Api.Generator/IsExternalInit.cs
+++ src/Atc.Rest.Api.Generator/IsExternalInit.cs
@@ -5,7 +5,7 @@ namespace System.Runtime.CompilerServices;
 /// Polyfill for init-only setters support in netstandard2.0.
 /// This type is required for C# 9 init-only setters to work in netstandard2.0.
 /// </summary>
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar", "S2094:Classes should not be empty", Justification = "Required polyfill for init-only setters in netstandard2.0")]
+[SuppressMessage("Sonar", "S2094:Classes should not be empty", Justification = "Required polyfill for init-only setters in netstandard2.0")]
 internal static class IsExternalInit
 {
 }
